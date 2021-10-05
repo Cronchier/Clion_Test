206de10f0b6ce6736a45c89bc701dcd49009b12a
Cronchier <67589269+Cronchier@users.noreply.github.com>
2021-10-05 18:21:47 +0200

    Integer Test

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+int main() {
+    int number;
 
-int main( )
-{
-    printf("Hello, World!\n");
+    printf("Enter an integer: ");
+
+    // reads and stores input
+    scanf("%d", &number);
+
+    // displays output
+    printf("You entered: %d", number);
+
     return 0;
 }
